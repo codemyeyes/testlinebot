46990fec45782b153ff988929ef22ba660612edd
Supasan Boonpreecha <codemyeyes@gmail.com>
2018-07-28 11:43:29 +0700

    send sticker

--- index.line_send_image.php
+++ index.line_send_image.php
@@ -5,8 +5,7 @@ require_once('./vendor/autoload.php');
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
 // use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-// use \LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
-use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
 
 $channel_token = '6Dp095D57jSNkHOBVUW/VxnZzTJeiNvMb8i4WKevKX51CXUx1Ww4Ym3qwMiqWFmrL3nr+5EZJwcU1UIN/JLawSiAyiCIfbVP7LSycYRx/QISOJDBqsjczyJUlSkS/kWOffmiGTzmIY02IZh/0lvRPwdB04t89/1O/w1cDnyilFU=';
 $channel_secret = 'ec9368121f4aa1a96f2be3bb7def2cd0';
@@ -22,16 +21,15 @@ if (!is_null($events['events'])) {
         //Get replyToken
         $replyToken = $event['replyToken']; 
 
-        // Sticker 
-        $packageId = 1; 
-        $stickerId = 410;
+        // Image
+        $originalContentUrl = 'https://cdn.shopify.com/s/files/1/1217/6360/products/Shinkansen_Tokaido_ShinFuji_001_1e44e709-ea47-41ac-91e4-89b2b5eb193a_grande.jpg?v=1489641827';
+        $previewImageUrl = 'https://cdn.shopify.com/s/files/1/1217/6360/products/Shinkansen_Tokaido_ShinFuji_001_1e44e709-ea47-41ac-91e4-89b2b5eb193a_grande.jpg?v=1489641827';
 
         $httpClient = new CurlHTTPClient($channel_token);
         $bot = new LINEBot($httpClient, array('channelSecret'=> $channel_secret));
         
         // $textMessageBuilder = new TextMessageBuilder($respMessage);
-        // $textMessageBuilder = new ImageMessageBuilder($originalContentUrl, $previewImageUrl);
-        $textMessageBuilder = new StickerMessageBuilder($packageId, $stickerId);
+        $textMessageBuilder = new ImageMessageBuilder($originalContentUrl, $previewImageUrl);
         $response=$bot->replyMessage($replyToken, $textMessageBuilder);
     }   
 }
