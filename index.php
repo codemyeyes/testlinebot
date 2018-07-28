f922ff79d39edcac0c209e5015ca8feeefa42041
Supasan Boonpreecha <codemyeyes@gmail.com>
2018-07-28 11:39:34 +0700

    fix

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ require_once('./vendor/autoload.php');
 // Namespace
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
-use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+// use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
 
 $channel_token = '6Dp095D57jSNkHOBVUW/VxnZzTJeiNvMb8i4WKevKX51CXUx1Ww4Ym3qwMiqWFmrL3nr+5EZJwcU1UIN/JLawSiAyiCIfbVP7LSycYRx/QISOJDBqsjczyJUlSkS/kWOffmiGTzmIY02IZh/0lvRPwdB04t89/1O/w1cDnyilFU=';
 $channel_secret = 'ec9368121f4aa1a96f2be3bb7def2cd0';
