95e1ea6b1e49c88317e4b437a5a4b31db4ac1511
Supasan Boonpreecha <codemyeyes@gmail.com>
2018-07-28 11:55:10 +0700

    init

--- index.line_send_sticker.php
+++ index.line_send_sticker.php
@@ -6,8 +6,7 @@ use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
 // use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 // use \LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
-// use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
-use \LINE\LINEBot\MessageBuilder\VideoMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
 
 $channel_token = '6Dp095D57jSNkHOBVUW/VxnZzTJeiNvMb8i4WKevKX51CXUx1Ww4Ym3qwMiqWFmrL3nr+5EZJwcU1UIN/JLawSiAyiCIfbVP7LSycYRx/QISOJDBqsjczyJUlSkS/kWOffmiGTzmIY02IZh/0lvRPwdB04t89/1O/w1cDnyilFU=';
 $channel_secret = 'ec9368121f4aa1a96f2be3bb7def2cd0';
@@ -19,17 +18,20 @@ $events=json_decode($content, true);
 if (!is_null($events['events'])) {
     //Loop through each event 
     foreach($events['events'] AS $event){
+
         //Get replyToken
         $replyToken = $event['replyToken']; 
 
-        // Video
-        $originalContentUrl = 'https://www.select2web.com.com/the-fuji.mp4'; 
-        $previewImageUrl = 'https://www.select2web.com.com/the-fuji.jpg';
+        // Sticker 
+        $packageId = 1; 
+        $stickerId = 410;
 
         $httpClient = new CurlHTTPClient($channel_token);
         $bot = new LINEBot($httpClient, array('channelSecret'=> $channel_secret));
         
-        $textMessageBuilder = new VideoMessageBuilder($originalContentUrl, $previewImageUrl);
+        // $textMessageBuilder = new TextMessageBuilder($respMessage);
+        // $textMessageBuilder = new ImageMessageBuilder($originalContentUrl, $previewImageUrl);
+        $textMessageBuilder = new StickerMessageBuilder($packageId, $stickerId);
         $response=$bot->replyMessage($replyToken, $textMessageBuilder);
     }   
 }
