0f72ddb4a79f95d37eb7540da0186d7136f3eace
Supasan Boonpreecha <codemyeyes@gmail.com>
2018-07-30 14:02:52 +0700

    fix

--- index.php
+++ index.php
@@ -35,7 +35,8 @@ if (!is_null($events['events'])) {
 
         $httpClient = new CurlHTTPClient($channel_token);
         $bot = new LINEBot($httpClient, array('channelSecret'=> $channel_secret));
-        $textMessageBuilder = new LocationMessageBuilder($title, $address, $latitude, $longitude);
+        
+        $textMessageBuilder = new TextMessageBuilder($title, $address, $latitude, $longitude);
         $response=$bot->replyMessage($replyToken, $textMessageBuilder);
     }   
 }
